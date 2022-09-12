c21623dfe445c60e02015e85e444c8cd5d0d4cc8
islomiy0010 <84896005+islomiy0010@users.noreply.github.com>
2022-09-12 16:14:18 +0500

    Add files via upload

---
+++ Amaliy ishlarim/4-lesson.py
@@ -0,0 +1,18 @@
+# first task
+# a=int(input('a sonni kiriting: '))
+# p=4*a
+# print(f' perimetr = {p}')
+
+# a=int(input('a sonni kiriting: '))
+# b=int(input('b sonni kiriting: '))
+# s=a*b
+# print(f'tortburchak yuzasi = {s}')
+
+# uzunlik=int(input('uzunlikni kiriting: '))
+# metr=uzunlik/100
+# print(f'uzunlik metrga otkazildi {metr}')
+
+a=int(input('a kesma: '))
+b=int(input('b kesma: '))
+nechMarta=a//b
+print(f' b kesma a kesmada {nechMarta} marta joylashgan')
